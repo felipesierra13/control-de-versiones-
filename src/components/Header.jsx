364b7de790cc9ecd417b03c925fef0e283c5f8ac
luis-felipe-sierra-franco <luisfelipesierrafranco@gmail.com>
2025-11-09 22:35:51 -0500

    feat(header): agregar componente header responsive

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header style={{ background: "#1e1e1e", padding: "12px", color: "white" }}>
+      <h1>Encabezado Responsivo ✅</h1>
+    </header>
+  );
+}
